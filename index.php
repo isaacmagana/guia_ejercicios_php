50d5c3db2a5f9ecb6de78e8d138454589bf7fa50
isaacmagana <6338179@clases.edu.sv>
2026-05-20 08:04:51 -0600

    Este archivo es el principal
    Este archivo contiene los links de todos los archivos

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+echo "<center><h1>Menu Principal</h1>";
+echo "<h3>";
+echo "<li><a href=ejercicio1.html>Ejercicio 1: Mensaje de Bienvenida</a></li>";
+echo "<li><a href=ejercicio2.html>Ejercicio 2: Suma de Dos Números</a></li>";
+echo "<li><a href=ejercicio3.html>Ejercicio 3: Resta de Dos Números</a></li>";
+echo "<li><a href=ejercicio4.html>Ejercicio 4: Multiplicación de Dos Números</a></li>";
+echo "<li><a href=ejercicio5.html>Ejercicio 5: División de Dos Números</a></li>";
+echo "<li><a href=ejercicio6.html>Ejercicio 6: Nombre y Edad</a></li>";
+echo "<li><a href=ejercicio7.html>Ejercicio 7: Determinar Número Mayor</a></li>";
+echo "<li><a href=ejercicio8.html>Ejercicio 8: Determinar Número Menor</a></li>";
+echo "<li><a href=ejercicio9.html>Ejercicio 9: Número Par o Impar</a></li>";
+echo "<li><a href=ejercicio10.html>Ejercicio 10: Promedio de Tres Notas</a></li>";
+echo "<li><a href=ejercicio11.html>Ejercicio 11: Promedio y Estado del Alumno</a></li>";
+echo "<li><a href=ejercicio12.html>Ejercicio 12: Área de un Rectángulo</a></li>";
+echo "<li><a href=ejercicio13.html>Ejercicio 13: Perímetro de un Cuadrado</a></li>";
+echo "<li><a href=ejercicio14.html>Ejercicio 14: Metros a Centímetros</a></li>";
+echo "<li><a href=ejercicio15.html>Ejercicio 15: Celsius a Fahrenheit</a></li>";
+echo "<li><a href=ejercicio16.html>Ejercicio 16: Cálculo de Salario Total</a></li>";
+echo "<li><a href=ejercicio17.html>Ejercicio 17: Aplicar Descuento del 10%</a></li>";
+echo "<li><a href=ejercicio18.html>Ejercicio 18: Factura con Subtotal y Total</a></li>";
+echo "<li><a href=ejercicio19.html>Ejercicio 19: Tabla de Multiplicar</a></li>";
+echo "<li><a href=ejercicio20.html>Ejercicio 20: Suma Consecutiva del 1 al N</a></li>";
+echo "<li><a href=ejercicio21.html>Ejercicio 21: Contador de Letras</a></li>";
+echo "<li><a href=ejercicio22.html>Ejercicio 22: Dólares a Euros</a></li>";
+echo "<li><a href=ejercicio23.html>Ejercicio 23: Calcular Total con IVA (13%)</a></li>";
+echo "<li><a href=ejercicio24.html>Ejercicio 24: Edad en 10 Años</a></li>";
+echo "<li><a href=ejercicio25.html>Ejercicio 25: Resumen de Datos de Usuario</a></li>";
+
