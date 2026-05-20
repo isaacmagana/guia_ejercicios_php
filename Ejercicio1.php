b4aad9a7df66031e21ec67f2940c9af3f5a05b07
isaacmagana <6338179@clases.edu.sv>
2026-05-20 08:08:49 -0600

    Ejercicio 1
    Elabore un formulario que solicite el nombre del usuario y envíe la información a
    otro archivo PHP donde se muestre un mensaje de bienvenida con el nombre
    ingresado.

---
+++ Ejercicio1.php
@@ -0,0 +1,10 @@
+<?php
+$nombre = $_POST ['nombre'];
+
+echo "<center><h1>Bienvenido</h1>";
+echo "Su nombre es:" . $nombre;
+echo "<div style='text-align: right; margin-top: 40px;'>";
+echo "<a href='index.php'>Volver al Menú Principal</a>";
+echo "</div>";
+
+?>
